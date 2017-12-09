596eb3e832458d2bbc0efd3052cb5443a0494c9e
Seth Shaw <seth@Seths-Computer.local>
2017-12-09 14:14:53 +0800

    Added motor control and more

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -10,9 +10,18 @@ int add(int a, int b) {
 
 class Robot: public IterativeRobot {
 public:
+  Talon *left_motor;
+  Talon *right_motor;
+  RobotDrive *my_robot;
+  Joystick *my_joy;
     Robot() { }
 
-    void RobotInit() { }
+    void RobotInit() {
+      left_motor = new Talon(left_motor_port);
+      right_motor = new Talon(right_motor_port);
+      my_robot = new RobotDrive(left_motor, right_motor);
+      my_joy = new Joystick(0)
+    }
 
     void DisabledInit() { }
     void AutonomousInit() { }
@@ -22,7 +31,9 @@ public:
     void DisabledPeriodic() { }
     void AutonomousPeriodic() { }
     void TeleopPeriodic() { }
-    void TestPeriodic() { }
+    void TestPeriodic() {
+      my_robot->TankDrive(0.5,0.5);
+     }
 };
 
 START_ROBOT_CLASS(Robot)
